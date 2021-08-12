19321d8933a51c6f6d6405e962af47ae4e40d43a
matias <matifandy@gmail.com>
2021-08-12 13:07:45 -0300

    footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,74 @@
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+    VisuallyHidden,
+  } from '@chakra-ui/react';
+  import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+  import { ReactNode } from 'react';
+  
+  const SocialButton = ({
+    children,
+    label,
+    href,
+  }: {
+    children: ReactNode;
+    label: string;
+    href: string;
+  }) => {
+    return (
+      <chakra.button
+        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+        rounded={'full'}
+        w={8}
+        h={8}
+        cursor={'pointer'}
+        as={'a'}
+        href={href}
+        display={'inline-flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        transition={'background 0.3s ease'}
+        _hover={{
+          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+        }}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    );
+  };
+  
+ const Footer: React.FC = () => {
+    return (
+      <Box
+        bg={useColorModeValue('brand.300', 'brand.300')}
+        color={useColorModeValue('white', 'white')}>
+        <Container
+          as={Stack}
+          maxW={'6xl'}
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ base: 'center', md: 'space-between' }}
+          align={{ base: 'center', md: 'center' }}>
+          <Text>© 2021 Matias Fandiño. All rights reserved</Text>
+          <Stack direction={'row'} spacing={6}>
+            <SocialButton label={'Twitter'} href={'#'}>
+              <FaTwitter />
+            </SocialButton>
+            <SocialButton label={'YouTube'} href={'#'}>
+              <FaYoutube />
+            </SocialButton>
+            <SocialButton label={'Instagram'} href={'#'}>
+              <FaInstagram />
+            </SocialButton>
+          </Stack>
+        </Container>
+      </Box>
+    );
+  }
+
+  export default Footer;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
+import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import News from '../../components/News';
 // import { Get } from '../../services/A
 const Home: React.FC = () => {
   // const [data, setData] = useState([])
-
 //   useEffect( () => {
 //       Get('/teams').then((res) => setData(res.data.data))  
-      
 // }, [])
 // console.log(data)
   return (
     <div className="Home">
       <Navbar />
       <News />
+      <Footer />
     </div>
   );
 }
